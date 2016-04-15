1e9b774fd8310404e0f4e07528e500784154ecdc
Matt Chandler <mcaz689@gmail.com>
2016-04-14 23:56:03 -0700

    remove cruft

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,6 @@ const PATHS = {
 };
 
 const common = {
-  // Entry accepts a path or an object of entries. We'll be using the
-  // latter form given it's convenient with more complex configurations.
   entry: {
     app: PATHS.app
   },
@@ -35,28 +33,15 @@ if (EVENT === 'start' || !EVENT) {
   module.exports = merge(common, {
     devServer: {
       contentBase: PATHS.build,
-
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
-
-      // Display only errors to reduce the amount of output.
       stats: 'errors-only',
-
-      // Parse host and port from env so this is easy to customize.
-      //
-      // If you use Vagrant or Cloud9, set
-      // host: process.env.HOST || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices unlike default
-      // localhost
       host: process.env.HOST,
       port: process.env.PORT
     },
+    devtool: 'eval-source-map',
     plugins: [
       new webpack.HotModuleReplacementPlugin()
     ]});

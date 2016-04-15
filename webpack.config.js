34efb25d1237925be41c49800c0355419e6c82fd
Matt Chandler <mcaz689@gmail.com>
2016-04-14 23:55:38 -0700

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,15 @@ const common = {
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
   }
 };
 
